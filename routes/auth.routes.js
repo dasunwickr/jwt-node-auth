996f9f867f6de8deb8e9842d1ed4358a55481acc
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-02-17 12:07:00 +0530

    chore: update auth route handlers

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,22 @@
 const { Router } = require("express");
+const authController = require("../controllers/auth.controller");
 
 const router = Router();
 
-router.get("/signup", (req, res) => {});
+router.get("/signup", (req, res) => {
+  authController.signup_get;
+});
 
-router.post("/signup", (req, res) => {});
+router.post("/signup", (req, res) => {
+  authController.signup_post;
+});
 
-router.get("/login", (req, res) => {});
+router.get("/login", (req, res) => {
+  authController.login_get;
+});
 
-router.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  authController.login_post;
+});
 
 module.exports = router;
