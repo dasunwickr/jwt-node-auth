38f259506068a10a4dc3cfec6427ad5ce30b6ce1
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-02-17 16:33:07 +0530

    feat: add user model

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,18 @@
+const User = require("../models/User");
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
 
-module.exports.signup_post = (req, res) => {
-  res.send("signup post");
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("User was not created");
+  }
 };
 
 module.exports.login_get = (req, res) => {
