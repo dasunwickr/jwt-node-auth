b6e6167883622a2f5f6d46d1815a660328efe965
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-02-17 12:04:45 +0530

    feat: add auth route handlers

---
+++ routes/auth.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/signup", (req, res) => {});
+
+router.post("/signup", (req, res) => {});
+
+router.get("/login", (req, res) => {});
+
+router.post("/login", (req, res) => {});
+
+module.exports = router;
