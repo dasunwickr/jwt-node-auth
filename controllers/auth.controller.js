27d3aba7cd5e3c99242593a11b56cb96b7b78fe4
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-02-20 08:52:37 +0530

    feat: add user login logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -50,8 +50,16 @@ module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
-  res.send("login post");
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
+  }
 };
